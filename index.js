4c76e5ea318bb7394bb558fd5afa8f267e9c8a02
Marlon Dantas <marlondantas2010@gmail.com>
2021-02-10 21:32:51 -0300

    hotfix para teste

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = express();
 
 // ALWAYS setup the alexa app and attach it to express before anything else.
 var alexaApp = new alexa.app("test");
+alexaApp.persistentSession = false;
 
 alexaApp.express({
   expressApp: app,
@@ -28,26 +29,63 @@ app.set("view engine", "ejs");
 
 alexaApp.launch(function(request, response) {
   //Intent Confirmation
+  request.getSession().set("Pontos", 0);
 
   var prompt = "Iniciando o jogo da Deusa da colheita";
-  
+
   response.say(prompt).shouldEndSession(false);
-}
-);
+  response.card("Oi mundo", "Isso é um exemplo de cartão...");
 
+});
 
+alexaApp.intent('NumeroMaior', {
+  "utterances": ["{O próximo número é maior}"]
+}, function(req, res) {
+  var session = request.getSession();
+  
+  var t_pontos = session.get("Pontos");
+  t_pontos = t_pontos + 1 ;
 
+  request.getSession().set("Pontos", t_pontos);
+
+  var saida = 'Você ganhou!';
+  res.say(saida).shouldEndSession(false);
+});
+
+alexaApp.intent('NumeroMenor', {
+  "utterances": ["{O próximo número é Menor}"]
+}, function(req, res) {
+
+  var session = request.getSession();
+  
+  var t_pontos = session.get("Pontos");
+  t_pontos = t_pontos - 1 ;
+  
+  request.getSession().set("Pontos", t_pontos);
+
+  var saida = 'Você perdeu!';
+  res.say(saida).shouldEndSession(false);
+});
+
+alexaApp.intent('TotalPontos', {
+  "utterances": ["{Quantos pontos eu tenho}"]
+}, function(req, res) {
+
+  var session = request.getSession();
+  var t_pontos = session.get("Pontos");
+  var saida = `Você tem ${t_pontos} pontos`;
+  res.say(saida).shouldEndSession(false);
+});
 
 
 alexaApp.intent('NameIntent', {
   "slots": { "NAME": "LITERAL", "AGE": "NUMBER" },
-  "utterances": ["{Meu nome é |meu nome} {matt|bob|bill|jake|nancy|mary|jane|NAME} e eu tenho {1-100|AGE}{anos de idade|}"]
+  "utterances": ["{Meu nome é |meu nome} {NAME} e eu tenho {1-100|AGE}{anos de idade}"]
 }, function(req, res) {
   res.say('Your name is ' + req.slot('NAME') + ' and you are ' + req.slot('AGE') + ' years old').shouldEndSession(false);
 });
 
-alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", "dawn"] };
-
+//alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", "dawn"] };
 
 alexaApp.intent('AgeIntent', {
   "slots": { "AGE": "NUMBER" },
@@ -60,24 +98,47 @@ alexaApp.intent('SelfIntent', {
   "slots": { "NAME": "LITERAL" },
   "utterances": ["me fale sobre o {puttareddy|NAME}"]
 }, function(req, res) {
+
   let name = req.data.request.intent.slots.NAME.value;
-  //console.log('name is -->', name)
+  console.log('name is -->', name)
+  
   let obj = '';
 
-  if (name === 'murali'){
-    obj +='Murali is Adolf Hitler for 235 Bloor East kids'
+  if (name === 'pokmemon'){
+    obj +='pokemon é bem legal'
   }else if(name === 'puttareddy'){
     obj +='Puttareddy is creazy boy in 235 Bloor east'
   }
 
   res.say(obj).shouldEndSession(false);
+
 });
 
-alexaApp.intent('AMAZON.CancelIntent', {
+alexaApp.intent('AMAZON.StopIntent', {
   "utterances": ["Fechar o app"]
 }, function(req, res) {
   res.say('Fechando o jogo da deusa da colheita.');
 });
 
+alexaApp.intent("AMAZON.HelpIntent", {
+  "utterances": ["O que app faz|Ajuda|help"]
+},
+function(request, response) {
+  var helpOutput = "´Bem vindo ao jogo da colheita, quando o jogo começar vai ser apresentado um número, você terá que descobrir se o próximo número é maior ou menor...";
+  var reprompt = "Gostaria de começar? Diga Começar o jogo ou fechar o app";
+  // AMAZON.HelpIntent must leave session open -> .shouldEndSession(false)
+  response.say(helpOutput).reprompt(reprompt).shouldEndSession(false);
+}
+);
+
+
+alexaApp.on('DeviceEngine.InputHandler', (request, response, request_json) => {
+  response.say("Deu gatinho do dispositivo " + request_json.request.event.deviceName);
+});
+
+alexaApp.error = function(exception, request, response) {
+  response.say("Desculpa, alguma coisa ruim aconteceu");
+};
 
 app.listen(PORT, () => console.log("Ouvindo a porta: " + PORT + "."));
+
