ae00fdc9a47db0a6ed1010bbdbc57332a492452c
Marlon Dantas <marlondantas2010@gmail.com>
2021-02-11 22:30:35 -0300

    hotfix do path

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+const path = require('path');
 var alexa = require("alexa-app");
 
 var PORT = process.env.PORT || 3000;
@@ -10,21 +10,10 @@ var alexaApp = new alexa.app("deusadacolheita");
 
 alexaApp.express({
   expressApp: app,
-  //router: express.Router(),
-
-  // verifies requests come from amazon alexa. Must be enabled for production.
-  // You can disable this if you're running a dev environment and want to POST
-  // things to test behavior. enabled by default.
   checkCert: false,
-
-  // sets up a GET route when set to true. This is handy for testing in
-  // development, but not recommended for production. disabled by default
   debug: true
 });
 
-// now POST calls to /test in express will be handled by the app.request() function
-
-// from here on you can setup any other express routes or middlewares as normal
 app.set('views', path.join(__dirname, '/alexaApp/views'))
 console.log(path.join(__dirname, '/alexaApp/views'));
 
